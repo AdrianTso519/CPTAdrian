be9236f61078150a5cf45508107c6e1c80572ff9
AdrianTso519 <adrian.tso26@ycdsbk12.ca>
2025-05-23 09:31:54 -0400

    Added con.printlns

--- CPTAdrian.java
+++ CPTAdrian.java
@@ -3,5 +3,9 @@ import arc.*;
 public class CPTAdrian{
 	public static void main(String[] args){
 		Console con = new Console("CPTAdrian - Video Poker", 1280, 720);
+		con.println("TEST");
+		con.println("HELLO");
+		con.println("CPT");
+		con.println("ICS3U1");
 	}
 }
